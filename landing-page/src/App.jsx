f67a2d8506046324d01aadfe6983244c2d5a95e4
Scott Milliorn <scottmilliorn@gmail.com>
2022-03-25 21:12:09 -0700

    anchor

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -4,30 +4,30 @@ import Marker from "./img/marker.svg";
 
 function App() {
   return (
-    <div className="container box-border p-0 m-0 font-sans">
+    <div className="container box-border p-0 m-0 font-sans leading-normal text-dark">
       <nav class="navbar">
         <div class="navbar-container">
           <div class="navbar-brand">
-            <a href="/">
+            <a className="text-black" href="/">
               <img src={Logo} alt="logo" />
             </a>
           </div>
 
           <ul class="navbar-nav-left">
             <li>
-              <a href="/">Menu</a>
+              <a className="text-black" href="/">Menu</a>
             </li>
             <li>
-              <a href="/">Rewards</a>
+              <a className="text-black" href="/">Rewards</a>
             </li>
             <li>
-              <a href="/">Gift Cards</a>
+              <a className="text-black" href="/">Gift Cards</a>
             </li>
           </ul>
 
           <ul class="navbar-nav-right">
             <li>
-              <a href="/">
+              <a className="text-black" href="/">
                 <img src={Marker} alt="" />
                 <span>Find a store</span>
               </a>
