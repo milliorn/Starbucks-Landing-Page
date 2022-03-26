acdd7a57d8081504acf3879e5a1bbe022456cba0
Scott Milliorn <scottmilliorn@gmail.com>
2022-03-26 16:37:55 -0700

    ul li

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -33,16 +33,16 @@ function App() {
 
           <ul className="flex items-center list-none navbar-nav-right">
             <li className="mx-4 my-0 font-bold">
-              <a className="text-black no-underline" href="/">
+              <a className="flex items-center text-black no-underline" href="/">
                 <img className="w-5 h-5 max-w-full mr-2" src={Marker} alt="" />
                 <span>Find a store</span>
               </a>
             </li>
-            <li className="mx-4 my-0 font-bold">
-              <button className="btn btn-dark-outline">Sign in</button>
+            <li className="mx-1 my-0 font-bold">
+              <button className="inline-block px-4 py-2 leading-5 text-center text-black no-underline border-2 border-black border-solid rounded-full cursor-pointer btn bg-none btn-dark-outline">Sign in</button>
             </li>
             <li className="mx-4 my-0 font-bold">
-              <button className="btn btn-dark">Join now</button>
+              <button className="inline-block px-4 py-2 leading-5 text-center text-white no-underline bg-black border-2 border-black border-solid rounded-full cursor-pointer btn bg-none btn-dark hover:bg-slate-700">Join now</button>
             </li>
           </ul>
 
