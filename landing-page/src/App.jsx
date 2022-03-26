66c21cf60b68db0909f238b3030d30c223e735aa
Scott Milliorn <scottmilliorn@gmail.com>
2022-03-25 21:43:33 -0700

    nav a

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -5,29 +5,29 @@ import Marker from "./img/marker.svg";
 function App() {
   return (
     <div className="box-border p-0 m-0 font-sans leading-normal text-dark">
-      <nav className="w-full h-auto p-5 bg-black navbar ">
+      <nav className="w-full h-auto p-5 navbar ">
         <div className="flex items-center justify-between mx-auto my-0 navbar-container max-w-screen-2xl">
           <div className="navbar-brand">
-            <a className="text-black" href="/">
+            <a className="text-black no-underline" href="/">
               <img className="max-w-full " src={Logo} alt="logo" />
             </a>
           </div>
 
           <ul className="flex items-center list-none navbar-nav-left">
             <li className="mx-4 my-0 font-bold">
-              <a className="text-black" href="/">Menu</a>
+              <a className="text-black no-underline" href="/">Menu</a>
             </li>
             <li className="mx-4 my-0 font-bold">
-              <a className="text-black" href="/">Rewards</a>
+              <a className="text-black no-underline" href="/">Rewards</a>
             </li>
             <li className="mx-4 my-0 font-bold">
-              <a className="text-black" href="/">Gift Cards</a>
+              <a className="text-black no-underline" href="/">Gift Cards</a>
             </li>
           </ul>
 
           <ul className="flex items-center list-none navbar-nav-right">
             <li className="mx-4 my-0 font-bold">
-              <a className="text-black" href="/">
+              <a className="text-black no-underline" href="/">
                 <img className="max-w-full " src={Marker} alt="" />
                 <span>Find a store</span>
               </a>
