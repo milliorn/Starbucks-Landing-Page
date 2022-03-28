a95f78d41182eca5bac82d205b26e9423c8d5153
Scott Milliorn <scottmilliorn@gmail.com>
2022-03-27 17:35:59 -0700

    boxb tweaks

--- landing-page/src/components/BoxB.jsx
+++ landing-page/src/components/BoxB.jsx
@@ -5,10 +5,10 @@ export function BoxB() {
     <section class="mb-8 text-white bg-rose-600 gap-8	items-center justify-between grid grid-cols-2">
       <img src={Img} alt="BoxB" className="max-w-full " />
       <div class="box-text max-w-md	text-center justify-self-center	py-5 px-0 my-0 mx-auto mb-8">
-        <h2 class="text-5xl	uppercase tracking-widest	font-semibold	">
+        <h2 class="text-6xl	uppercase tracking-widest	font-semibold	leading-snug	">
           New to the nice list
         </h2>
-        <p class="text-2xl mb-5 leading-7	">
+        <p class="text-2xl mb-5 leading-relaxed	py-5 tracking-wider">
           For a nondairy twist on a holiday cookie classic, try the new Iced
           Sugar Cookie Almond Milk Latte.
         </p>
